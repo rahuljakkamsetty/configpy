470c15579a1cce7a2243c42ed424e2cc81b2572a
Rahul Jakkamsetty <rahul.jakkamsetty@dfki.de>
2024-03-12 13:25:18 +0100

    added json utils

--- configpy/configure.py
+++ configpy/configure.py
@@ -42,9 +42,40 @@ myconfig()
 ```
 """
 
-from typing import Any
+from typing import Any, NewType
 from collections.abc import Sequence
 from collections import OrderedDict
+import json
+Path = NewType('Path', str)
+
+
+def load_json(path: "Path") -> Any:
+    """loads a json file.
+    
+    Parameters:
+    -----------
+    path: file path of json to be loaded.
+    
+    """
+    with open(path, 'r+') as f:
+        data = json.load(f)
+    return data
+
+
+def write_json(path: Path, data: Any) -> None:
+    """writes data to a json file.
+    
+    Parameters:
+    -----------
+    path: file path of json to be saved.
+    data: data to be dumped to json file.
+
+    """
+    assert path.endswith(
+        '.json'), f"invalid path {path}, path should end with '.json'"
+    with open(path, 'w+') as f:
+        json.dump(data, f, indent=4)
+
 
 
 class Parameters(OrderedDict):
