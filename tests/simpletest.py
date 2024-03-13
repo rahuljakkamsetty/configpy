a1a73d57d50703cf5e946130893881c3fad813fa
Rahul Jakkamsetty <rahul.jakkamsetty@dfki.de>
2024-03-13 14:37:17 +0100

    added json loading to tests

--- tests/simpletest.py
+++ tests/simpletest.py
@@ -18,10 +18,14 @@ myconfig = ConfigBuild(obj = add,
                                                             a = 4,
                                                             b = 2)))
 
+json_config = ConfigBuild.from_json('./config.json')
+
 class TestConfig(unittest.TestCase):
     def test_bodmas(self):
         """Test the operations on give numbers"""
         self.assertEqual(myconfig(), 10+(10-4*2))
+    def test_json(self):
+        self.assertDictEqual(json_config(), {'a': 10, 'b': 2})
 
 
 if __name__ == "__main__":
